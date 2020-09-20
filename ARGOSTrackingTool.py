55f0b7201434bedc4d939d11a38af1ccea1af2a9
nec18 <69816644+nec18@users.noreply.github.com>
2020-09-20 16:20:08 -0400

    Initial commit of ARGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Niki Cleary (niki.cleary@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------------------------
+
